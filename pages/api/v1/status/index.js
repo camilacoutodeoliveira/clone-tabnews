e5bad12e92675eea0f9562f2c28a01ff985fdb94
Camila Couto de Oliveira <camilacoutodeoliveira@gmail.com>
2025-11-18 21:27:45 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const dbConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rowCount;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: dbOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
